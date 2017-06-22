b82ce1c9e6e8674117be6498e7a2a58f97bc0462
dandanBrave <269045975@qq.com>
2017-06-22 20:37:39 +0800

    提交第四道题

---
+++ 重建二叉树.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/6/22.
+ */
+function TreeNode(x) {
+ this.val = x;
+ this.left = null;
+ this.right = null;
+ }
+function reConstructBinaryTree(pre, vin)
+{
+    if(pre.length==0 || vin.length==0){
+        return null;
+    }
+    //前序第一个为根节点，也是中序左右子树的分割点
+    var index = vin.indexOf(pre[0]);
+    var left = vin.slice(0,index);
+    var right = vin.slice(index+1);
+    return{
+        val : pre[0],
+        left: reConstructBinaryTree(pre.slice(1,index),left),
+        right: reConstructBinaryTree(pre.slice(index+1),right)
+    };
+}
+module.exports = {
+    reConstructBinaryTree : reConstructBinaryTree
+};
