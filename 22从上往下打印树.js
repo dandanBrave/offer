deb77478b82f25a0bf56fd3e7721b46a4d44b89f
dandanBrave <269045975@qq.com>
2017-07-21 08:56:24 +0800

    提交第二十二道题第二次

--- 22从上往下打印树.js
+++ 22从上往下打印树.js
@@ -1,37 +1,34 @@
-/**
- * Created by Administrator on 2017/7/20.
- */
 function TreeNode(x) {
- this.val = x;
- this.left = null;
- this.right = null;
- }
+    this.val = x;
+    this.left = null;
+    this.right = null;
+}
 function PrintFromTopToBottom(root) {
     //当结点非空时才进行操作
-    if(!root) return;
+    if(!root) return [];
     //用于存放还未遍历的元素
-    var queue = [];
-    //用于记录当前处理的结点
-    var treeNode;
+    var q = [];
     //用于记录需要打印的结点值
     var array = [];
     //将根节点入队
-    queue.push(root);
+    q.push(root);
     //队列非空则进行处理
-    while (queue.length()>0){
+    while (q.length>0){
         //删除队首元素
-        treeNode = queue.shift();
+        var node = q.shift();
         //将需要打印的结点值存入数组
-        array.push(treeNode.val);
+        array.push(node.val);
+
         //如果左子节点不为空，则左子节点入队
-        if(treeNode.left){
-            queue.push(treeNode.left);
+        if(node.left){
+            q.push(node.left);
         }
         //如果右子节点不为空，则右子节点入队
-        if(treeNode.right){
-            queue.push(treeNode.right);
+        if(node.right){
+            q.push(node.right);
         }
     }
+    return array;
 }
 module.exports = {
     PrintFromTopToBottom : PrintFromTopToBottom
